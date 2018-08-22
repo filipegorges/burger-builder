cd5573d88fea5fc265d33ad68a773942442ed044
Filipe Gorges Reuwsaat <lipirr@hotmail.com>
2018-08-21 21:48:40 -0300

    Add burger ingredient dynamic stacking

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,19 @@ import Auxiliar from '../../hoc/Auxiliar';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0,
+        },
+    }
+
     render() {
         return(
             <Auxiliar>
-                <Burger />
+                <Burger ingredients={this.state.ingredients} />
                 <div>Basic Controls</div>
             </Auxiliar>    
         );
