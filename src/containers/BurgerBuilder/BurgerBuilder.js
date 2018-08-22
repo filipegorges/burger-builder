927fb82e43e5e30065c981c9fd69f2ee3ce6625b
Filipe Gorges Reuwsaat <lipirr@hotmail.com>
2018-08-21 21:37:35 -0300

    Add Burger CSS and build Burger example

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import Auxiliar from '../../hoc/Auxiliar';
+import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
     render() {
         return(
             <Auxiliar>
-                <div>Burger</div>
+                <Burger />
                 <div>Basic Controls</div>
             </Auxiliar>    
         );
