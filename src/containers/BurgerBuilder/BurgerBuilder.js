fe8175c4f0837fa5d6d2412047b40aaa9012871d
Filipe Gorges Reuwsaat <lipirr@hotmail.com>
2018-08-22 22:07:01 -0300

    Add modal trigger

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -22,6 +22,7 @@ class BurgerBuilder extends Component {
         },
         totalPrice: 4,
         purchasable: false,
+        purchasing: false,
     }
 
     updatePurchaseState (updatedIngredients) {
@@ -69,6 +70,10 @@ class BurgerBuilder extends Component {
         this.updatePurchaseState(updatedIngredients);
     } 
 
+    purchaseHandler = () => {
+        this.setState({ purchasing: true });
+    }
+
     render() {
         const disabledInfo = {
             ...this.state.ingredients
@@ -80,7 +85,7 @@ class BurgerBuilder extends Component {
 
         return(
             <Auxiliar>
-                <Modal>
+                <Modal show={this.state.purchasing}>
                     <OrderSummary ingredients={this.state.ingredients} />
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
@@ -89,6 +94,7 @@ class BurgerBuilder extends Component {
                     ingredientRemoved={this.removeIngredientHandler}
                     disabled={disabledInfo}
                     purchasable={this.state.purchasable}
+                    ordered={this.purchaseHandler}
                     price={this.state.totalPrice} />
             </Auxiliar>    
         );
