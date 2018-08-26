15bbc50a8d348f560b5949f98616732553cb8969
Filipe Gorges Reuwsaat <lipirr@hotmail.com>
2018-08-26 00:37:37 -0300

    Implement Order fetching

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,16 +1,46 @@
 import React, { Component } from 'react';
 
+import axios from '../../axios-orders';
+
 import Order from '../../components/Order/Order';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 class Orders extends Component {
+    state = {
+        orders: [],
+        loading: true,
+    }
+
+    componentDidMount() {
+        axios.get('/orders.json')
+            .then(response => {
+                let fetchedOrders = [];
+                for (let key in response.data) {
+                    fetchedOrders.push({
+                        ...response.data[key],
+                        id: key,
+                    });
+                }
+                this.setState({orders: fetchedOrders, loading: false});
+            })
+            .catch(error => {
+                console.log(error);
+                this.setState({loading: false})
+            });
+    }
+
     render() {
         return (
             <div>
-                <Order />
-                <Order />
+                {this.state.orders.map(order => (
+                    <Order 
+                        key={order.id}
+                        ingredients={order.ingredients}
+                        price={order.price} />
+                ))}
             </div>
         );
     }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
