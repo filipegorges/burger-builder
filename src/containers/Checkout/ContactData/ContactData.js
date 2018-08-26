97541797586ce81ce40d52db84ffe980304b1469
Filipe Gorges Reuwsaat <lipirr@hotmail.com>
2018-08-26 02:26:38 -0300

    Add custom validation to form fields

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -17,6 +17,10 @@ class ContactData extends Component {
                     placeholder: 'Your Name'
                 }, 
                 value: '',
+                validation: {
+                    required: true
+                },
+                valid: false,
             },
             street: {
                 elementType: 'input',
@@ -25,6 +29,10 @@ class ContactData extends Component {
                     placeholder: 'Street'
                 }, 
                 value: '',
+                validation: {
+                    required: true
+                },
+                valid: false,
             },
             zipCode: {
                 elementType: 'input',
@@ -33,6 +41,12 @@ class ContactData extends Component {
                     placeholder: 'ZIP Code'
                 }, 
                 value: '',
+                validation: {
+                    required: true,
+                    minLength: 8,
+                    maxLength: 9,
+                },
+                valid: false,
             },
             country: {
                 elementType: 'input',
@@ -41,6 +55,10 @@ class ContactData extends Component {
                     placeholder: 'Country'
                 }, 
                 value: '',
+                validation: {
+                    required: true
+                },
+                valid: false,
             },
             email: {
                 elementType: 'input',
@@ -49,6 +67,10 @@ class ContactData extends Component {
                     placeholder: 'Your E-Mail'
                 }, 
                 value: '',
+                validation: {
+                    required: true
+                },
+                valid: false,
             },
             deliveryMethod: {
                 elementType: 'select',
@@ -86,6 +108,25 @@ class ContactData extends Component {
             });
     }
 
+    checkValidity(value, rules) {
+        let isValid = true;
+
+        if (rules.required) {
+            isValid = value.trim() !== '' && isValid;
+        }
+
+        if (rules.minLength) {
+            isValid = value.length >= rules.minLength && isValid;
+        }
+
+        if (rules.maxLength) {
+            isValid = value.length <= rules.maxLength && isValid;
+        }
+
+
+        return isValid;
+    }
+
     inputChangedHandler = (event, inputIdentifier) => {
         const updatedOrderForm = {
             ...this.state.orderForm
@@ -94,6 +135,7 @@ class ContactData extends Component {
             ...updatedOrderForm[inputIdentifier]
         };
         updatedFormElement.value = event.target.value;
+        updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
         updatedOrderForm[inputIdentifier] = updatedFormElement;
         this.setState({orderForm: updatedOrderForm});
     }
