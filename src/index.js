2b0b162c694b9464b1edc1528d92de9213277def
Filipe Gorges Reuwsaat <lipirr@hotmail.com>
2018-08-31 01:32:53 -0300

    Add auto logout

--- src/index.js
+++ src/index.js
@@ -11,10 +11,12 @@ import App from './App';
 
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import orderReducer from './store/reducers/order';
+import authReducer from './store/reducers/auth';
 
 const rootReducer = combineReducers({
     burgerBuilder: burgerBuilderReducer,
     order: orderReducer,
+    auth: authReducer,
 });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -8,10 +8,11 @@ export const authStart = () => {
     };
 };
 
-export const authSuccess = (authData) => {
+export const authSuccess = (token, userId) => {
     return {
         type: actionTypes.AUTH_SUCCESS,
-        authData: authData,
+        idToken: token,
+        userId: userId,
     };
 };
 
@@ -19,6 +20,20 @@ export const authFail = error => {
     return {
         type: actionTypes.AUTH_FAIL,
         error: error,
+    };
+};
+
+export const logout = () => {
+    return {
+        type: actionTypes.AUTH_LOGOUT
+    };
+};
+
+export const checkAuthTimeout = expirationTime => {
+    return dispatch => {
+        setTimeout(() => {
+            dispatch(logout);
+        }, expirationTime * 1000);
     };
 };
 
@@ -38,11 +53,12 @@ export const auth = (email, password, isSignup) => {
         axios.post(url, authData)
             .then(response => {
                 console.log(response);
-                dispatch(authSuccess(response.data));
+                dispatch(authSuccess(response.data.idToken, response.data.localId));
+                dispatch(checkAuthTimeout(response.data.expiresIn));
             })
             .catch(error => {
                 console.log(error);
-                dispatch(authFail(error));
+                dispatch(authFail(error.response.data.error));
             })
     };
 };
