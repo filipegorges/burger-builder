a032d2237f509ede53491e5bb7b35160aee28a0b
Filipe Gorges Reuwsaat <lipirr@hotmail.com>
2018-08-30 23:37:10 -0300

    Refactor redux behavior

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
 
-import * as actions from '../../store/actions/index';
-
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from './ContactData/ContactData';
 
